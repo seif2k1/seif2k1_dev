5c5071f5e8e0b0971f72206a8b7a7136901cab04
seif2k1 <seif2kghost@gmail.com>
2024-02-02 23:22:27 +0100

    3

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/seif2k_dev/",
+  base: "/seif2k1_dev/",
 });
